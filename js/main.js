a5ee5ec085e955a1b638cc1576d2de72de7c7188
Hwngmai <hungmai0203@gmail.com>
2022-06-06 16:10:53 +0700

    add feedback slider & team section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+// numberGenerator
 jQuery(document).ready(function( $ ) {
     $('.counter').counterUp({
         delay: 20,
         time: 1000 
     });
     });
+// owlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
